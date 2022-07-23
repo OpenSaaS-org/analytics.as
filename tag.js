38aa47c382ff1115009a2528f3e4634b9b7ca5fd
Nathan Clevenger <nateclev@gmail.com>
2022-07-23 05:29:33 -0500

    Update tag.js

--- tag.js
+++ tag.js
@@ -1,1 +1,10 @@
-
+export default {
+  fetch: async (req, env) => {
+    const { hostname } = new URL(req.url)
+    const [tenant] = hostname.split('.')
+    
+    return new Response(`
+      const body = await fetch('https://workers.cloudflare.com/cf.json').then(res => res.json())
+      const beacon = await fetch(\`http://${tenant}.ılıl.to/api/beacon\`, { method: POST, body }).then(res => res.json())
+      `, { headers: { 'content-type': 'application/js' }})
+}
