f19d35613420b50d678400ff868e1fb090455c7e
Nathan Clevenger <nateclev@gmail.com>
2022-07-24 12:30:20 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -11,10 +11,25 @@ export default {
     }
     
     return new Response(`
-      const body = ${JSON.stringify(meta)}
-      fetch('https://workers.cloudflare.com/cf.json')
-        .then(res => res.json())
-        .then(body => fetch('https://workers.cloudflare.com/cf.json', { method: POST, body }))
-        .then(fetch('http://${tenant}.ılıl.to/api/beacon/${id}', { method: POST, body })
-        `, { headers: { 'content-type': 'application/js' }})
+<script>
+  // Initialize the agent at application startup.
+  // You can also use https://openfpcdn.io/fingerprintjs/v3/esm.min.js
+  const fpPromise = import('https://openfpcdn.io/fingerprintjs/v3')
+    .then(FingerprintJS => FingerprintJS.load())
+
+  // Get the visitor identifier when you need it.
+  fpPromise
+    .then(fp => fp.get())
+    .then(result => console.log(result.visitorId))
+    
+  const body = ${JSON.stringify(meta)}
+  fetch('https://workers.cloudflare.com/cf.json')
+    .then(res => res.json())
+    .then(body => fetch('https://workers.cloudflare.com/cf.json', { method: POST, body }))
+    .then(fetch('http://${tenant}.ılıl.to/api/beacon/${id}', { method: POST, body })
+    `, { headers: { 'content-type': 'application/js' }})
+    
+</script>
+`
+  }
 }
